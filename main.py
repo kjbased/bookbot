cbf84c4858dd7062aa4cc9d61a61a60decf0a8b7
kjbased <kjbass@outlook.com>
2024-11-13 10:52:09 -0700

    Updated main.py functions to include text reader, word counter, and letter counter

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ def main():
     book_path = "books/frankenstein.txt"
     text = bookText(book_path)
     wordCount = numWords(text)
+    letterCount = numLetters(text)
+
     #print (text)
     print ("---------------------------")
     print (f"Total words: {wordCount}" )
+    print ("---------------------------")
+    print (letterCount)
 
 def bookText(book_path):
     with open (book_path) as f:
@@ -12,5 +16,18 @@ def bookText(book_path):
 
 def numWords(text):
     words = text.split()
+    #print (words)
     return len(words)
+
+def numLetters(text):
+    letterCount = {}
+    for chars in text:
+        lowercase = chars.lower()
+        if lowercase in letterCount:
+            letterCount[lowercase] += 1
+        else:
+            letterCount[lowercase] = 1        
+    return letterCount
+    pass
+
 main()
